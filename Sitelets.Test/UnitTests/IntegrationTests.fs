8c1255af2429b630a3e0560a74fdc90ee69cf9ca
András Jankó <andras.janko@gmail.com>
2021-08-24 17:31:39 +0200

    add HelloWorld integration test

---
+++ Sitelets.Test/UnitTests/IntegrationTests.fs
@@ -0,0 +1,35 @@
+﻿module IntegrationTests
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy;
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Sitelets
+open NUnit.Framework
+open FsUnit
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Mvc.Testing
+open Sitelets
+
+[<Test>]
+let ``Hello World integration test`` () =
+    let webAppFactory = new WebApplicationFactory<ANCFSharp.Startup>()
+    
+    let client = webAppFactory.CreateClient()
+
+    async {
+        let! resp = client.GetAsync("/hello") |> Async.AwaitTask
+        resp.IsSuccessStatusCode |> should be True
+
+        let! resp = client.GetAsync("/nothello") |> Async.AwaitTask
+        resp.IsSuccessStatusCode |> should be False
+    }
+    |> Async.StartAsTask
